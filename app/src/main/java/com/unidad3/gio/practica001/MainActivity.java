3eb612ca8383af6bc7c525728d9b00ba3bec755d
gio salazar <36206915+GioAmeca@users.noreply.github.com>
2018-03-13 10:24:49 -0600

    1

---
+++ app/src/main/java/com/unidad3/gio/practica001/MainActivity.java
@@ -0,0 +1,26 @@
+package com.unidad3.gio.practica001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+EditText cuadro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cuadro=(EditText) findViewById(R.id.TxtPalabra);
+    }
+//sintaxis para el metodo de boton
+    public void obtener(View v){
+       String palabra= String.valueOf(cuadro.getText());
+       palabra=palabra.toUpperCase();
+        StringBuilder pala=new StringBuilder(palabra);
+
+        pala.reverse();
+
+        cuadro.setText(pala);
+}
+}
